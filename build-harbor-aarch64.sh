466ec0b9d11a5e7df2f7ff3e4eb3d5478f1eb0b2
吴亚明 <yaming.wu1@gientech.com>
2025-09-29 19:19:12 +0800

    修改路径

--- build-harbor-aarch64.sh
+++ build-harbor-aarch64.sh
@@ -10,12 +10,12 @@ sed -i "s#dev-arm#${GIT_BRANCH}-aarch64#g" harbor-arm/Makefile
 cd harbor-arm
 git clone --branch ${GIT_BRANCH} https://github.com/goharbor/harbor.git src/github.com/goharbor/harbor
 pwd
-cp -f ../harbor/Makefile src/github.com/goharbor/harbor/
-cp -f ../harbor/make/photon/Makefile src/github.com/goharbor/harbor/make/photon/
-cp -f ../harbor/make/photon/portal/Dockerfile src/github.com/goharbor/harbor/make/photon/portal/
-cp -f ../harbor/make/photon/registry/builder src/github.com/goharbor/harbor/make/photon/registry/
-cp -f ../harbor/make/photon/registry/redis.patch src/github.com/goharbor/harbor/make/photon/registry/
-cp -f ../harbor/src/portal/src/app/shared/components/about-dialog/about-dialog.component.html src/github.com/goharbor/harbor/src/portal/src/app/shared/components/about-dialog/
+cp -f ./harbor/Makefile src/github.com/goharbor/harbor/
+cp -f ./harbor/make/photon/Makefile src/github.com/goharbor/harbor/make/photon/
+cp -f ./harbor/make/photon/portal/Dockerfile src/github.com/goharbor/harbor/make/photon/portal/
+cp -f ./harbor/make/photon/registry/builder src/github.com/goharbor/harbor/make/photon/registry/
+cp -f ./harbor/make/photon/registry/redis.patch src/github.com/goharbor/harbor/make/photon/registry/
+cp -f ./harbor/src/portal/src/app/shared/components/about-dialog/about-dialog.component.html src/github.com/goharbor/harbor/src/portal/src/app/shared/components/about-dialog/
 
 # compile redis
 make compile_redis
